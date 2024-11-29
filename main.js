be636d239d11b3ee081dbe9711b467a643d625da
romain goré <romaingore070204@gmail.com>
2024-11-29 22:45:11 +0100

    supp anim like

--- main.js
+++ main.js
@@ -1,15 +1,3 @@
 import './style.css'
 
-const heartBtn = document.getElementById('heart-btn');
-const heartIcon = document.getElementById('heart-icon');
-
-heartBtn.addEventListener('click', () => {
-    // Utilise getAttribute pour obtenir le chemin relatif du fichier image
-    if (heartIcon.getAttribute('src') === './assets/images/heart.svg') {
-        heartIcon.setAttribute('src', './assets/images/heart-plein.svg'); // Remplace par l'image remplie
-    } else {
-        heartIcon.setAttribute('src', './assets/images/heart.svg'); // Remplace par l'image vide
-    }
-});
-
 
