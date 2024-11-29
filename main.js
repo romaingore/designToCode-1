0db19b867e92d3bf8bddce947ae74a8bb31ec826
romain goré <romaingore070204@gmail.com>
2024-11-29 22:42:38 +0100

    change url

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ const heartIcon = document.getElementById('heart-icon');
 
 heartBtn.addEventListener('click', () => {
     // Utilise getAttribute pour obtenir le chemin relatif du fichier image
-    if (heartIcon.getAttribute('src') === './public/images/heart.svg') {
-        heartIcon.setAttribute('src', './public/images/heart-plein.svg'); // Remplace par l'image remplie
+    if (heartIcon.getAttribute('src') === './assets/images/heart.svg') {
+        heartIcon.setAttribute('src', './assets/images/heart-plein.svg'); // Remplace par l'image remplie
     } else {
-        heartIcon.setAttribute('src', './public/images/heart.svg'); // Remplace par l'image vide
+        heartIcon.setAttribute('src', './assets/images/heart.svg'); // Remplace par l'image vide
     }
 });
 
